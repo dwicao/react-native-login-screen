814164aa00b3231efaf46e47b8ee6ea6ed918ca2
dwicao <lutfiandwica@gmail.com>
2016-12-29 11:10:00 +0700

    aniamted width button

--- src/components/ButtonSubmit.js
+++ src/components/ButtonSubmit.js
@@ -2,34 +2,61 @@ import React, { Component, PropTypes } from 'react';
 import Dimensions from 'Dimensions';
 import {
 	StyleSheet,
-	TouchableHighlight,
+	TouchableOpacity,
 	Text,
+	Animated,
+	Easing,
 } from 'react-native';
 
 export default class ButtonSubmit extends Component {
+	constructor() {
+		super();
+
+		this._onPress = this._onPress.bind(this);
+		this.buttonAnimated = new Animated.Value(0);
+	}
+
+	_onPress() {
+		this.buttonAnimated.setValue(0);
+		Animated.timing(
+			this.buttonAnimated,
+			{
+				toValue: 1,
+				duration: 600,
+				easing: Easing.linear
+			}
+		).start();
+	}
+
 	render() {
+		const DEVICE_WIDTH = Dimensions.get('window').width;
+		const moveWidth = this.buttonAnimated.interpolate({
+	    inputRange: [0, 1],
+	    outputRange: [DEVICE_WIDTH - 40, 80]
+	  });
+
 		return (
-			<TouchableHighlight style={styles.button}>
-				<Text style={styles.text}>LOGIN</Text>
-			</TouchableHighlight>
+			<Animated.View style={{width: moveWidth}}>
+				<TouchableOpacity style={styles.button}
+					onPress={this._onPress}
+					activeOpacity={0.7} >
+						<Text style={styles.text}>LOGIN</Text>
+				</TouchableOpacity>
+			</Animated.View>
 		);
 	}
 }
-
-const DEVICE_WIDTH = Dimensions.get('window').width;
-const DEVICE_HEIGHT = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
 	button: {
 		alignItems: 'center',
 		justifyContent: 'center',
 		backgroundColor: '#F035E0',
-		width: DEVICE_WIDTH - 40,
 		height: 40,
 		marginTop: 10,
 		borderRadius: 20,
 	},
 	text: {
 		color: '#ffffff',
-	}
+	},
 });
