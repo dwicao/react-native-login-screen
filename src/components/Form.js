a9cd327bf9f20f6dd92539d057126a5ece5a4088
ahmadahmadi14 <ahmadahmadi140989@gmail.com>
2017-04-04 07:24:39 +0700

    add show password

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,12 @@ import {
 	StyleSheet,
 	KeyboardAvoidingView,
 	View,
+	ActivityIndicator,
+	TouchableOpacity,
+	Image,
 } from 'react-native';
+
+const eye = require('../../eye_black.png');
 
 import UserInput from './UserInput';
 import ButtonSubmit from './ButtonSubmit';
@@ -14,6 +19,17 @@ import usernameImg from '../images/username.png';
 import passwordImg from '../images/password.png';
 
 export default class Form extends Component {
+	constructor(props) {
+    super(props);
+    this.state = {
+			showPass: true,
+		};
+	}
+
+	showPass() {
+  this.state.press === false ? this.setState({ showPass: false, press: true }) :this.setState({ showPass: true, press: false });
+  }
+
 	render() {
 		return (
 			<KeyboardAvoidingView behavior='padding'
@@ -24,11 +40,18 @@ export default class Form extends Component {
 					returnKeyType={'done'}
 					autoCorrect={false} />
 				<UserInput source={passwordImg}
-					secureTextEntry={true}
+					secureTextEntry={this.state.showPass}
 					placeholder='Password'
 					returnKeyType={'done'}
 					autoCapitalize={'none'}
 					autoCorrect={false} />
+					<TouchableOpacity
+						activeOpacity={0.7}
+						style={styles.btnEye}
+						onPress={() => this.showPass()}
+					>
+						<Image source={eye} style={styles.iconEye} />
+					</TouchableOpacity>
 			</KeyboardAvoidingView>
 		);
 	}
@@ -41,5 +64,15 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		alignItems: 'center',
-	}
+	},
+	btnEye: {
+    position: 'absolute',
+    top: 55,
+    right: 28,
+  },
+  iconEye: {
+    width: 25,
+    height: 25,
+    tintColor: 'rgba(0,0,0,0.2)',
+  },
 });
